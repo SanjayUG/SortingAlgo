5cadf5db09c238ad1aaa5a08aaa4bdba027ad2ac
SanjayUG <ganjekarsanjay6@gmail.com>
2024-07-11 11:16:50 +0530

    first commit

---
+++ src/components/footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+// Footer Component
+const Footer = () => {
+    return (
+        <footer>
+            <p>
+                
+            </p>
+        </footer>
+    );
+}
+ 
+export default Footer;
